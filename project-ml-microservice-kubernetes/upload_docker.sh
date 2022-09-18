c4f81074347cda649003fd8d4d690e81f6e4d1a7
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-09-18 18:16:43 +0000

    Made necessary changes

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,18 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-dockerpath=
+
+dockerpath=mlapp/app
+
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+
+docker tag mlapp:latest dulgan/mlapp:latest
+
+docker login --username=dulgan
+
+docker push dulgan/mlapp:latest
