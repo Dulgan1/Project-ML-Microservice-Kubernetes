d5c2585d98d26377f8488604f845bc7a67311d63
Dulgan Danjuma <dulgandanjuma@gmail.com>
2022-09-20 15:45:11 +0000

    Finished

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,14 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+dockerpath=mlapp/app
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run mlapp --image=dulgan/mlapp 
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward mlapp 8000:80
